aad0e18136e4a465d8b3bd764800befa50d8c8a5
monikatl <monikatl@interia.pl>
2023-10-18 09:35:37 +0200

    Rest controllers

--- project/src/main/java/com/warehouse/project/controller/ContractorController.java
+++ project/src/main/java/com/warehouse/project/controller/ContractorController.java
@@ -1,4 +1,12 @@
 package com.warehouse.project.controller;
 
+import com.warehouse.project.WarehouseNotFoundException;
+import com.warehouse.project.data.Commodity;
+import com.warehouse.project.repository.CommodityRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ContractorController {
 }
