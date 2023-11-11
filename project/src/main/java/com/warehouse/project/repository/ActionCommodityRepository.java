059cba36555c64244a37a467807df6785fabd10f
monikatl <monikatl@interia.pl>
2023-11-11 16:22:54 +0100

    Delete project Spring Boot app

--- project/src/main/java/com/warehouse/project/repository/ActionCommodityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.warehouse.project.repository;
-
-import com.warehouse.project.data.ActionCommodity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ActionCommodityRepository extends JpaRepository<ActionCommodity, Long> {
-}
--- project/src/main/java/com/warehouse/project/repository/CommodityRepository.java
+++
@@ -1,8 +0,0 @@
-package com.warehouse.project.repository;
-
-import com.warehouse.project.data.Commodity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommodityRepository extends JpaRepository<Commodity, Long> {
-
-}
